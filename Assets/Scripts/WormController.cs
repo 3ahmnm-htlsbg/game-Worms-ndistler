6df0b56ce3afbd37166045e3cfed8cc8f2decb87
niklasdistler <niklas.distler03@htl-salzburg.ac.at>
2020-01-21 15:04:23 +0100

    Add WormController Script

---
+++ Assets/Scripts/WormController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormController : MonoBehaviour
+{
+    public int x;
+    public string text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (x > 0)
+        {
+            Debug.Log("x ist größer als 0");
+        }
+        else
+        {
+            Debug.Log("x ist kleiner als 0");
+        }
+
+        if (text == "ndistler")
+        {
+            Debug.Log("In der Text Variable steht ndistler");
+        }
+        else
+        {
+            Debug.Log("In der Text Variable steht etwas anderes");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
