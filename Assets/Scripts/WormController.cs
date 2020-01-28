7a3ca291e07945444d95d826801436e0c7d369f3
niklasdistler <niklas.distler03@htl-salzburg.ac.at>
2020-01-28 15:07:39 +0100

    made an if function for a GetKeyDown

--- Assets/Scripts/WormController.cs
+++ Assets/Scripts/WormController.cs
@@ -4,46 +4,12 @@ using UnityEngine;
 
 public class WormController : MonoBehaviour
 {
-    public int x;
-    //public string text;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        /*
-        if (x > 0)
-        {
-            Debug.Log("x ist größer als 0");
-        }
-        else
-        {
-            Debug.Log("x ist kleiner als 0");
-        }
-
-        if (text == "ndistler")
-        {
-            Debug.Log("In der Text Variable steht ndistler");
-        }
-        else
-        {
-            Debug.Log("In der Text Variable steht etwas anderes");
-        }*/
-
-        if (x > 10)
-        {
-            Debug.Log("x ist größer als 10");
-        }
-
-        else
-        {
-            Debug.Log("x ist kleiner als 10");
-        }
-    }
-
-
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("key W is pressed");
+        }
     }
 }
