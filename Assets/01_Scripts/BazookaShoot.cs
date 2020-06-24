a6ebe1ff967c1d0f8a70ab4735248306526ff1f8
niklasdistler <niklas.distler03@htl-salzburg.ac.at>
2020-06-24 22:22:44 +0200

    Distler Abgabe

---
+++ Assets/01_Scripts/BazookaShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BazookaShoot : MonoBehaviour
+{
+    private GameObject bulletInst;
+
+    public GameObject bullet;
+
+    public GameObject bulletSpawnCube;
+
+    private Vector3 gunCubeSpawnPos;
+
+    private Quaternion gunCubeSpawnRot;
+
+    public void Shoot()
+    {
+        gunCubeSpawnPos = bulletSpawnCube.transform.position;
+        gunCubeSpawnRot = bulletSpawnCube.transform.rotation;
+        bulletInst = Instantiate(bullet, gunCubeSpawnPos, gunCubeSpawnRot) as GameObject;
+        Rigidbody rbShoot = bulletInst.GetComponent<Rigidbody>();
+        rbShoot.AddForce(transform.up * -1000f);
+        
+    }
+
+}
